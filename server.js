40c254e663ff565f5aad5ec996ad02ac2bf83772
Olulana Kolawole <kolawolegolulana@gmail.com>
2017-03-20 18:54:38 +0200

    make a difference

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 app.use(express.static(path.resolve(__dirname, 'www')));
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function() {
- console.log("Listening to port", app.get("port"));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
